fbb5b7e8424ff29d54badf1a270c2bb1e180b8f2
Shahad3004 <shahadferas2@gmail.com>
2025-05-11 00:01:32 +0300

    Add footer and header and worked on the movies and home pagesn

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to My Movies</h1>
+      <p>Explore our movies!</p>
+    </div>
+  );
+};
+
+export default Home;
